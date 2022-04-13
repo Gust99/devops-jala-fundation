ac1753f770b3626cae9719b49048d1cc50b013e1
student9 jala-foundation <student9@jala-foundation.org>
2022-04-13 16:54:34 -0400

    Fibonacci

--- a.js
+++ a.js
@@ -1,10 +1,11 @@
 console.log("Fibonacci numbers");
 
-let a = 1;
+let a = 0;
 let b = 1;
 let aux = 0;
+
 for(let i = 0; i < 50; i++) {
-  console.log(a+b);
+  console.log(b);
   aux = a;
   a = b;
   b = aux + b;
